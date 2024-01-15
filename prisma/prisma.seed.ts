2dea670a9da2907d0ae5db12d920a323cd24656b
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-15 15:21:55 +0700

    🔷 update seeder

--- prisma/prisma.seed.ts
+++ prisma/prisma.seed.ts
@@ -2,11 +2,21 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const role = prisma.user_types.createMany({
-    data: [{ type: 'guest' }, { type: 'free' }, { type: 'paid' }],
-  });
-
-  return role;
+  return await prisma.$transaction([
+    prisma.user_types.createMany({
+      data: [{ type: 'guest' }, { type: 'free' }, { type: 'paid' }],
+    }),
+    prisma.users.create({
+      data: {
+        first_name: 'user',
+        last_name: 'guest',
+        user_type: 'guest',
+        email: 'guest@example.com',
+        password: 'itsNotAPassword',
+        user_uid: 'e19a18f4-b37d-11ee-a506-0242ac120002',
+      },
+    }),
+  ]);
 }
 main()
   .then(async () => {
