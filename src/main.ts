fd5aee8e053734c1ed1e04d04c0e5251692a762e
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-15 13:02:44 +0700

    🔷 update config

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
-const config = new ConfigService();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(config.get('APP_PORT') || 3000);
 }
 bootstrap();
