18865230e23777bd3279907e93c1e2161d60c808
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-15 10:23:14 +0700

    🔷 add config and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
+const config = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(config.get('APP_PORT') || 3000);
 }
 bootstrap();
