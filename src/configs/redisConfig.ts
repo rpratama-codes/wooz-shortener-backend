0e6e36e31be38fd98e07c218af783142e1b5935a
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-20 14:16:16 +0700

    ✅ add redis

---
+++ src/configs/redisConfig.ts
@@ -0,0 +1,22 @@
+import { CacheModuleAsyncOptions } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
+
+export const redisConfig: CacheModuleAsyncOptions = {
+  isGlobal: true,
+  imports: [ConfigModule],
+  useFactory: async (config: ConfigService) => {
+    const store = await redisStore({
+      socket: {
+        host: config.get<string>('REDIS_HOST'),
+        port: parseInt(config.get<string>('REDIS_PORT')),
+      },
+      password: config.get<string>('REDIS_PASSWORD'),
+      ttl: parseInt(config.get<string>('REDIS_CACHE_TTL')) || 30,
+    });
+    return {
+      store: () => store,
+    };
+  },
+  inject: [ConfigService],
+};
